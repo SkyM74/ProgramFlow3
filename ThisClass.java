4f5a26fd3ff31a969b99edddd716b3465a137201
SkyM74 <sky.masori@mrjaffesclass.com>
2022-09-22 13:56:08 -0700

    commit

---
+++ ThisClass.java
@@ -0,0 +1,28 @@
+
+/**
+ * Write a description of class CoolClass here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class ThisClass
+{
+
+    int a = 5;
+    int b = 2;
+    int c = 1;
+    int d = -8;
+    
+    public void run() {
+        System.out.println("Hello World!");
+    }
+    
+    
+    public static void main(String[] args)
+    {        
+        ThisClass hw = new ThisClass();
+        hw.run();
+    }
+}
+
+
